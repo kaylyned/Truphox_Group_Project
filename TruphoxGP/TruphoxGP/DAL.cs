fa9353d4dbebd6b340048063da72c80d88fbc97c
dan.courcelles <dan.courcelles@outlook.com>
2018-10-16 14:47:51 -0500

    Working on DAL.

--- TruphoxGP/TruphoxGP/DAL.cs
+++ TruphoxGP/TruphoxGP/DAL.cs
@@ -2,10 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace TruphoxGP
 {
     public class DAL
     {
+        string _connnectionString { get; set; }
+        string _command { get; set; }
+
+        SqlConnection _Connection;
+
+        struct parms
+        {
+            public string _p;
+            public string _v;
+        }
+
+        List<parms> _parms = new List<parms>();
+
+        public DAL(string command)
+        {
+            _command = command;
+            _connnectionString = ConfigurationManager.ConnectionStrings["dal"].ConnectionString;
+            _Connection = new SqlConnection(_connnectionString);
+        }
+
+        public void addParm(string p, string v)
+        {
+            parms myParms = new parms();
+            myParms._p = p;
+            myParms._v = p;
+            _parms.Add(myParms);
+        }
+
+        public void execNonQuery()
+        {
+
+        }
+
     }
 }

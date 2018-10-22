6b1f465828423a6e48b6f84cb995b0943ad29b6b
Kaylyn Dunham <Kaylyn.ED@hotmail.com>
2018-10-22 14:03:35 -0500

    search glyphicon.

--- TruphoxGP/TruphoxGP/MasterSupreme.Master.designer.cs
+++ TruphoxGP/TruphoxGP/MasterSupreme.Master.designer.cs
@@ -13,15 +13,6 @@ namespace TruphoxGP {
     public partial class Site1 {
         
         /// <summary>
-        /// btnSearch control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnSearch;
-        
-        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
@@ -29,6 +20,15 @@ namespace TruphoxGP {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// btnSearch control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton btnSearch;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
